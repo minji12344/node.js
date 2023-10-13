7ff3ed2f053d513f113f03e05bdab101203f62a9
minji12344 <140360630+minji12344@users.noreply.github.com>
2023-10-13 18:25:39 +0900

    Add files via upload

---
+++ 1013/views/routes/books.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db');
+
+/* 검색페이지 */
+router.get('/', function(req, res, next) {
+    res.render('index', {title:'도서검색', pageName:'books/search.ejs'})
+});
+
+// 도서검색 결과 저장
+router.post('/search/insert', function(req,res){
+const title = req.body.title;
+const authors = req.body.authors;
+const price=req.body.price;
+const publisher=req.body.publisher;
+const image = req.body.thumbnail;
+const contents=req.body.contents;
+const isbn=req.body.isbn;
+//console.log(title,authors,price,publisher,image,contents);
+
+const sql1='select * from books where isbn=?';
+db.get().query(sql1, [isbn], function(err, rows){
+    if(err) console.log('isbn검색', err); // 에러가있을경우 확인하는 방법
+    if(rows.length > 0){ // 이미도서가 등록된 경우
+        result='1';
+        res.send('1');
+    }else{ // 도서가 없는 경우
+        const sql='insert into books(title, authors, price, publisher, image, contents, isbn) values(?,?,?,?,?,?,?)';
+        db.get().query(sql, [title, authors, price, publisher, image, contents, isbn], function(err, rows){
+            if(err) console.log('도서저장 : ', err);
+            res.send('0');
+        });
+    }
+    });
+});
+
+// 도서목록 JSON
+router.get('/list.json', function(req, res){
+    const page=req.query.page;
+    const start=(parseInt(page)-1) * 5;
+    const sql='select * from books order by bid desc limit ?,5';
+    db.get().query(sql, [start], function(err, rows){
+        if(err) console.log('도서목록 JSON : ', err);
+        res.send(rows);
+    });
+});
+
+// 도서목록 페이지 이동
+router.get('/list', function(req, res){
+    res.render('index', {title:'도서목록', pageName:'books/list.ejs'});
+});
+
+//데이터갯수
+router.get('/count', function(req, res){
+    const sql='select count(*) total from books';
+    db.get().query(sql, function(err, rows){
+        res.send(rows[0]);
+    });
+});
+module.exports = router;
---
+++ 1013/views/routes/users.js
@@ -0,0 +1,128 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../db'); //하나 올라가서 가져오므로 ../
+var multer=require('multer');
+//업로드함수
+var upload = multer({
+    storage:multer.diskStorage({
+        destination:(req,file, done) => {
+            done(null,'./public/upload/photo')
+        },
+        filename:(req, file, done)=>{
+            var fileName=Date.now() + ".jpg";
+            done(null, fileName);
+        }
+    })
+});
+
+
+/* 사용자목록 페이지  */
+router.get('/', function(req, res, next) {
+    res.render('index', {title:'사용자목록', pageName:'users/list.ejs'})
+});
+
+//사용자목록 JSON
+router.get('/list.json', function(req, res){
+    const sql='select * from users order by uname'; //order by 정렬
+    db.get().query(sql, function(err, rows){
+        res.send(rows);
+    });
+});
+
+
+//로그인페이지
+router.get('/login', function(req,res){
+    res.render('index', {title:'로그인', pageName:'users/login.ejs'})
+});
+
+//로그인 체크
+router.post('/login', function(req,res){ //post -> req 요청
+    const uid=req.body.uid;
+    const upass=req.body.upass;
+    const sql='select * from users where uid=?';
+    db.get().query(sql, [uid], function(err, rows){ // get() 함수라서 소괄호/ query 메소드/ [uid] 배열값 /에러, 결과
+        let result='0'; //초기값 (아이디가 없다)
+        if(rows[0]){//id가 있으면?
+            if(rows[0].upass == upass){
+                result = '1'; //성공
+            }else{
+                result = '2';
+            }
+        }
+        res.send(result);
+    });
+});
+
+// 회원가입페이지 이동
+router.get('/insert', function(req,res){
+    res.render('index',{title:'회원가입', pageName:'users/insert.ejs'})
+});
+
+//회원가입
+router.post('/insert', function(req,res){
+    const uid=req.body.uid;
+    const upass=req.body.upass;
+    const uname=req.body.uname;
+    const phone=req.body.phone;
+    const address1=req.body.address1;
+    const address2=req.body.address2;
+    console.log(uid, upass, uname, phone, address1, address2);
+    const sql='insert into users(uid, upass, uname, phone, address1, address2) values(?,?,?,?,?,?)';
+    db.get().query(sql,[uid, upass, uname, phone, address1, address2], function(err, rows){
+        res.redirect('/users/login');
+    });
+});
+
+// 마이페이지 이동
+router.get('/mypage', function(req,res){ 
+    const uid=req.query.uid;
+    const sql='select * from users where uid=?';
+    db.get().query(sql, [uid], function(err,rows){
+        //console.log('..............', rows[0]);
+        res.render('index', {title:'마이페이지', pageName:'users/mypage.ejs', user:rows[0]});
+    });
+});
+
+//수정페이지 이동
+router.get('/update', function(req,res){
+    const uid=req.query.uid;
+    const sql='select * from users where uid=?';
+    db.get().query(sql, [uid], function(err,rows){
+        //console.log('..............', rows[0]);
+        res.render('index', {title:'정보수정', pageName:'users/update.ejs', user:rows[0]});
+    });
+});
+
+//정보수정하기
+router.post('/update', upload.single('file'), function(req,res){
+    const uname = req.body.uname;
+    const uid= req.body.uid;
+    const phone=req.body.phone;
+    const address1=req.body.address1;
+    const address2=req.body.address2;
+    let photo=req.body.photo;
+    if(req.file) photo=req.file.filename;
+    const sql='update users set uname=?,phone=?,address1=?,address2=?, photo=? where uid=?';
+    db.get().query(sql,[uname,phone,address1,address2,photo,uid], function(err, rows){
+        if(err) console.log('sql 오류 ..............', err); //sql에서 틀린 것 찾아내기 위한 코드
+        res.redirect('/users/mypage?uid=' + uid);
+    });
+});
+
+//비밀번호변경 페이지 이동  // get 가져오는, 연동시켜주는  post데이터수정
+router.get("/change", function(req, res) {
+    res.render("index", {title:"비밀번호변경", pageName:"users/change.ejs"});
+});
+
+//비밀번호 변경
+router.post("/change", function(req, res){
+    const uid=req.body.uid;
+    const upass=req.body.npass;
+    const sql='update users set upass=? where uid=?';
+    db.get().query(sql, [upass,uid],function(err,rows){
+        if(err) console.log(err);
+        res.sendStatus(200);
+    });
+});
+
+module.exports = router;
